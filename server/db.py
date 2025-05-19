c67e9a2575807246b8afdb32126c0228735dcf03
Jonathan <jkwan14@gmail.com>
2025-05-19 15:25:04 -0400

    Test commit

---
+++ server/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/pantry"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
