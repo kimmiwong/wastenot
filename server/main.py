cbdb6df69afa0679f0912495ab4eead4ae18949f
Rebecca <rebeccalh01@gmail.com>
2025-05-20 15:40:00 -0400

    fixed CORS and added basic list to home.jsx

--- server/main.py
+++ server/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from schema import FoodIn, FoodOut, FoodUpdate
 import db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/pantry")
