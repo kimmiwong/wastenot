b995ead926eb61f24213b71d0c4044572d87e4ff
Rebecca <rebeccalh01@gmail.com>
2025-05-20 14:37:31 -0400

    some backend changes to have sample data in the database

--- server/main.py
+++ server/main.py
@@ -1,18 +1,20 @@
-from fastapi import FastAPI
-from schema import FoodIn, FoodOut, FoodUpdate
 from fastapi import FastAPI, HTTPException
+from schema import FoodIn, FoodOut, FoodUpdate
 import db
 
 app = FastAPI()
+
 
 @app.get("/api/pantry")
 async def get_items() -> list[FoodOut]:
     return db.get_items()
 
+
 @app.post("/api/pantry")
-async def create_item(item: FoodIn ) -> FoodOut:
+async def create_item(item: FoodIn) -> FoodOut:
     item = db.create_item(item)
     return item
+
 
 @app.put("/api/pantry/{id}")
 async def update_item(id: int, item: FoodUpdate):
@@ -22,7 +24,7 @@ async def update_item(id: int, item: FoodUpdate):
     return updated_item
 
 
-@app.delete("/api/pantry{id}")
+@app.delete("/api/pantry/{id}")
 async def delete_item(id: int):
     deleted_item = db.delete_item(id)
     if not deleted_item:
