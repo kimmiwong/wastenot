10e58f7bfe3945395f5fd1779916652448ccc819
Jonathan <jkwan14@gmail.com>
2025-05-23 16:26:03 -0400

    selectedIngredients function not working

--- client/src/components/ShowItems.jsx
+++ client/src/components/ShowItems.jsx
@@ -1,191 +1,201 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import EditModal from './EditModal';
-import EditItem from './EditItem';
-import ItemTable from './ItemTable';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import EditModal from "./EditModal";
+import EditItem from "./EditItem";
+import ItemTable from "./ItemTable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 export default function ShowItems() {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [selectedItem, setSelectedItem] = useState(null);
-    const [selectedIngredient, setSelectedIngredient] = useState([]);
-    const [sortedButton, setSortedButton] = useState(false);
-    const [sortedData, setSortedData] = useState([]);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedIngredient, setSelectedIngredient] = useState([]);
+  const [sortedButton, setSortedButton] = useState(false);
+  const [sortedData, setSortedData] = useState([]);
 
+  function openEdit(itemId) {
+    setSelectedItem(itemId);
+  }
 
-    function openEdit(itemId) {
-        setSelectedItem(itemId)
+  function closeEdit() {
+    setSelectedItem(null);
+  }
 
+  function sortItems(data) {
+    if (sortedButton) {
+      data.sort((a, b) => {
+        return new Date(a.expiration_date) - new Date(b.expiration_date);
+      });
+    } else {
+      data.sort((a, b) => {
+        return a.name.localeCompare(b.name, undefined, { sensitivity: "base" });
+      });
     }
 
-    function closeEdit() {
-        setSelectedItem(null)
+    return data;
+  }
 
-    }
-
-    function sortItems(data) {
-        console.log("!", data);
-        if (sortedButton) {
-            data.sort((a, b) => {
-                return new Date(a.expiration_date) - new Date(b.expiration_date);
-            });
-            console.log("data sorted", data)
+  async function deleteItem(id) {
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/food-items/${id}`,
+        {
+          method: "DELETE",
         }
-        console.log(sortedButton);
-        console.log("date", data, "sorted date", sortedData)
-        return sortedData;
+      );
+
+      if (!response.ok) throw new Error("Failed to delete food item");
+
+      const newData = data.filter((item) => item.id !== id);
+      setData(newData);
+    } catch (error) {
+      console.error("Error deleting food item", error);
     }
+  }
 
-    async function deleteItem(id) {
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const res = await fetch("http://localhost:8000/api/food-items");
+      const json = await res.json();
+      setData(json);
 
-        try {
-            const response = await fetch(`http://localhost:8000/api/food-items/${id}`, {
-                method: 'DELETE'
-            })
-
-            if (!response.ok) throw new Error('Failed to delete food item')
-
-            const newData = data.filter((item) => item.id !== id);
-            setData(newData)
-
-        }
-
-        catch (error) {
-            console.error('Error deleting food item', error)
-
-        }
-
+      setSortedData(json);
+    } catch (error) {
+      setError(error);
+      console.error("Error fetching items", error);
+    } finally {
+      setIsLoading(false);
     }
-
-    const fetchData = async () => {
-        try {
-            setIsLoading(true)
-            const res = await fetch('http://localhost:8000/api/food-items')
-            const json = await res.json();
-            setData(json);
-            console.log("data", data)
-            setSortedData(json);
-            console.log("Sorted Data", sortedData)
-
-        } catch (error) {
-            setError(error);
-            console.error("Error fetching items", error)
-        } finally {
-            setIsLoading(false)
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    if (error) {
-        return <p>Something went wrong. Please refresh.</p>
+  };
+  // Needs to be fixed
+  const selectedIngredients = (itemName, isChecked) => {
+    console.log(itemName, isChecked);
+    if (isChecked) {
+      return setSelectedIngredient(...selectedIngredient, itemName);
+    } else {
+      return selectedIngredient.filter((item) => item !== itemName);
     }
+  };
 
-    if (isLoading) {
-        return <p>Loading...</p>
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <div className="item-container">
-            <button type="button" onClick={() => setSortedButton(!sortedButton)}>Sort by {sortedButton === true ? 'Name' : 'Expiration Date'}
-            </button>
-            <div className="pantry-items">
-                <h3>Pantry Items</h3>
-                <ItemTable>
-                    {sortItems(data).map((item) =>
+  if (error) {
+    return <p>Something went wrong. Please refresh.</p>;
+  }
 
-                        item.category_id === 1 ? (
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-                            <tr key={item.id}>
-                                <input
-                                    className="checkbox"
-                                    type="checkbox"
-                                    onChange={(e) => setSelectedIngredient(e.target.value)}
-                                />
-                                <td>{item.name}</td>
-                                <td>{item.expiration_date}</td>
-                                <td>
-                                    <button
-                                        type="button"
-                                        onClick={() => deleteItem(item.id)}
-                                        className="delete-button"
-                                        aria-label="Delete item"
-                                    >
-                                        <FontAwesomeIcon icon={faTrashCan} />
-                                    </button>
+  return (
+    <div className="item-container">
+      <button type="button" onClick={() => setSortedButton(!sortedButton)}>
+        Sort by {sortedButton === true ? "Name" : "Expiration Date"}
+      </button>
+      <div className="pantry-items">
+        <h3>Pantry Items</h3>
+        <ItemTable>
+          {sortItems(data).map((item) =>
+            item.category_id === 1 ? (
+              <tr key={item.id}>
+                <td>
+                  <input
+                    className="checkbox"
+                    type="checkbox"
+                    onChange={(e) =>
+                      selectedIngredients(item.name, e.target.checked)
+                    }
+                  />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.expiration_date}</td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={() => deleteItem(item.id)}
+                    className="delete-button"
+                    aria-label="Delete item"
+                  >
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </button>
 
+                  <button
+                    type="button"
+                    onClick={() => openEdit(item.id)}
+                    aria-label="Edit item"
+                  >
+                    <FontAwesomeIcon icon={faEdit} />
+                  </button>
+                </td>
+              </tr>
+            ) : null
+          )}
 
-                                    <button type="button" onClick={() => openEdit(item.id)} aria-label="Edit item">
-                                        <FontAwesomeIcon icon={faEdit} />
-                                    </button>
+          {selectedItem && (
+            <EditModal onClose={closeEdit}>
+              <EditItem
+                fetchItems={fetchData}
+                id={selectedItem}
+                closeModal={closeEdit}
+              />
+            </EditModal>
+          )}
+        </ItemTable>
+      </div>
 
-                                </td>
+      <div className="fridge-items">
+        <h3>Fridge Items</h3>
+        <ItemTable>
+          {sortItems(data).map((item) =>
+            item.category_id === 2 ? (
+              <tr key={item.id}>
+                <td>
+                  <input
+                    className="checkbox"
+                    type="checkbox"
+                    onChange={(e) => setSelectedIngredient(e.target.value)}
+                  />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.expiration_date}</td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={() => deleteItem(item.id)}
+                    className="delete-button"
+                    aria-label="Delete item"
+                  >
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </button>
 
-                            </tr>
-                        ) : null
-
-                    )}
-
-                    {selectedItem && (
-                        <EditModal onClose={closeEdit}>
-                            <EditItem fetchItems={fetchData} id={selectedItem} closeModal={closeEdit} />
-                        </EditModal>
-                    )}
-                </ItemTable>
-            </div>
-
-
-            <div className="fridge-items">
-                <h3>Fridge Items</h3>
-                <ItemTable>
-                    {sortItems(data).map((item) =>
-
-                        item.category_id === 2 ? (
-
-                            <tr key={item.id}>
-                                <input
-                                    className="checkbox"
-                                    type="checkbox"
-                                    onChange={(e) => setSelectedIngredient(e.target.value)}
-                                />
-                                <td>{item.name}</td>
-                                <td>{item.expiration_date}</td>
-                                <td>
-                                    <button
-                                        type="button"
-                                        onClick={() => deleteItem(item.id)}
-                                        className="delete-button"
-                                        aria-label="Delete item"
-                                    >
-                                        <FontAwesomeIcon icon={faTrashCan} />
-                                    </button>
-
-
-                                    <button type="button" onClick={() => openEdit(item.id)} aria-label="Edit item">
-                                        <FontAwesomeIcon icon={faEdit} />
-                                    </button>
-
-                                </td>
-
-
-                            </tr>
-                        ) : null
-
-                    )}
-                    {selectedItem && (
-                        <EditModal onClose={closeEdit}>
-                            <EditItem fetchItems={fetchData} id={selectedItem} closeModal={closeEdit} />
-                        </EditModal>
-                    )}
-                </ItemTable>
-            </div>
-        </div>
-
-    );
-};
+                  <button
+                    type="button"
+                    onClick={() => openEdit(item.id)}
+                    aria-label="Edit item"
+                  >
+                    <FontAwesomeIcon icon={faEdit} />
+                  </button>
+                </td>
+              </tr>
+            ) : null
+          )}
+          {selectedItem && (
+            <EditModal onClose={closeEdit}>
+              <EditItem
+                fetchItems={fetchData}
+                id={selectedItem}
+                closeModal={closeEdit}
+              />
+            </EditModal>
+          )}
+        </ItemTable>
+      </div>
+    </div>
+  );
+}
