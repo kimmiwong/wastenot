bba6f2ae9ebbd63b25461e1829b5ca35179baf07
Lindsey <lawsonlindsey36@yahoo.com>
2025-05-29 16:37:54 -0400

    added trash can icon to favorites and title

--- client/src/pages/Favorites.jsx
+++ client/src/pages/Favorites.jsx
@@ -1,15 +1,19 @@
 import SimpleHeader from "../components/Header";
 import { useFavorites } from "../context/FavoritesContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Favorites() {
-  const { selectedFavorites } = useFavorites();
+  const { selectedFavorites, deleteFavorite } = useFavorites();
+
   return (
     <div>
       <SimpleHeader />
       <ul>
         {selectedFavorites.map((recipe) => (
           <li key={recipe.id}>
+            <h3 className="fav-title">{recipe.title}</h3>
             <img src={recipe.image} alt={recipe.title} />
             <a
               href={recipe.sourecUrl}
@@ -18,6 +22,10 @@ export default function Favorites() {
             >
               View Recipe
             </a>
+            <button onClick={() => deleteFavorite(recipe.id)}>
+              <FontAwesomeIcon icon={faTrashCan} />
+            </button>
+
           </li>
         ))}
       </ul>
