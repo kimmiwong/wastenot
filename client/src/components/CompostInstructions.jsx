5965c7c311e73c1bd49a52abb2eded322a1da67e
Kimmi <189289679+kimmiwong@users.noreply.github.com>
2025-05-30 13:53:01 -0400

    fixed zoom in google maps and added composting details

---
+++ client/src/components/CompostInstructions.jsx
@@ -0,0 +1,27 @@
+export default function CompostInstructions () {
+    console.log("compost instructions")
+
+    return (
+<>
+    <h1>How to get started with composting</h1>
+    <p>
+        For taking compost to a collection site:
+        <ol>
+            <li>You will need a large container with a tight-fitting lid. Some people prefer to keep their compost in the freezer to reduce any unpleasant smells.</li>
+            <li>Place all of your food scraps in the compost container. Fruits, vegetables, grains, eggshells, and coffee grounds are acceptable for composting at every location. Check your local dropoff site for guidance on meat, fish, and cooking oils.</li>
+            <li>Bring your compost to the dropoff location when your bin is full. Once a week is recommended to avoid attracting pests.</li>
+        </ol>
+        For composting at home:
+        <ol>
+            <li>There are many methods of composting at home. Some involve purchasing materials such as a tumbler or a worm bin. The method described here is the most economical one.</li>
+            <li>Choose an area of your yard that is not accessible to children or pets. Create a bin that can hold a large amount of compost using materials such chicken wire and fenceposts. Recycling materials that you already have on hand is a great way to make this project even more sustainable.</li>
+            <li>Using a container with a tight-fitting lid, collect your kitchen scraps and take them to the compost pile every few days. It is not generally recommended to compost meat and fish products at home as you may attract insects and wild animals to your yard.</li>
+            <li>Regularly add non-food material to your compost pile. This includes grass clippings, leaves, and straw and will help keep any smells or insects away.</li>
+            <li>Turn your compost pile using a pitchfork around once a month to increase aeration. This will help speed up decomposition.</li>
+            <li>When the compost looks like soil, it is decomposed enough that you can use it in your garden or flowerbeds.</li>
+        </ol>
+    </p>
+</>
+
+    )
+}
