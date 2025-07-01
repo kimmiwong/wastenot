ad0b3f4ae7072c7f04bd64d9864f42e794fef679
Kimmi <189289679+kimmiwong@users.noreply.github.com>
2025-07-01 19:45:21 -0400

    fixed cors again

--- server/main.py
+++ server/main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="https://www.wastenotkitchen.com",
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
