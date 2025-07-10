10639800d869ecc152a908fbcd081c6b471fcd46
Lindsey <lawsonlindsey36@yahoo.com>
2025-07-10 10:48:09 -0400

    hamburger menu added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,19 @@
-import { Box } from "@mantine/core";
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import WasteNotLogo from "../assets/WasteNotLogo.png";
-import classes from "./Header.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { useNotifications } from "../context/NotificationsContext";
 import compostLogo from "../assets/compostLogo.PNG";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBell, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { useNotifications } from "../context/NotificationsContext";
 import { useUser } from "../context/UserProvider";
+import classes from "./Header.module.css";
 
 export default function SimpleHeader() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [notifOpen, setNotifOpen] = useState(false);
   const dropdownRef = useRef(null);
   const { notifications, fetchNotifications } = useNotifications();
+  const { user } = useUser();
 
   useEffect(() => {
     fetchNotifications();
@@ -22,95 +22,84 @@ export default function SimpleHeader() {
   useEffect(() => {
     const handleClickOutside = (e) => {
       if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-        setIsOpen(false);
+        setNotifOpen(false);
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
-  const { user } = useUser();
   return (
-    <Box>
-      <header className={classes.header}>
-        <div className={classes.inner}>
-          <div className={classes.leftSection}>
-            <Link to="/Home">
-              <img
-                src={WasteNotLogo}
-                alt="WasteNot Logo"
-                className={classes.logo}
-              />
-            </Link>
-            <span className={classes.logoText}>WasteNot</span>
-          </div>
-          <div className="right-section">
-            <div className="welcome">
-              {user ? (
-                <>
-                  <p>
-                    Welcome, <b>{user.username}</b>!
-                  </p>
-                  <Link to="/logout">Logout</Link>
-                </>
-              ) : (
-                <>
-                  <p>You are not logged in.</p>
-                </>
+    <header className={classes.header}>
+      <div className={classes.inner}>
+        <div className={classes.leftSection}>
+          <Link to="/Home">
+            <img src={WasteNotLogo} alt="Logo" className={classes.logo} />
+          </Link>
+          <span className={classes.logoText}>WasteNot</span>
+        </div>
+
+        <div className={classes.rightSection}>
+          <div className={classes.notifications} ref={dropdownRef}>
+            <div
+              className={classes.dropdownButton}
+              onClick={() => setNotifOpen((prev) => !prev)}
+            >
+              {notifications.length > 0 && (
+                <span className={classes.notification}>
+                  ({notifications.length})
+                </span>
               )}
-            </div>
-            <div className="icon-tooltip-wrapper">
-              <Link to="/Compost" className="compost-link">
-                <img
-                  src={compostLogo}
-                  alt="Compost Link"
-                  className="compost-logo"
-                />
-                <span className="tooltip-text">Compost Locations</span>
-              </Link>
-            </div>
-            <div className="fav-wrapper icon-tooltip-wrapper">
-              <Link to="/Favorites" className="favorite-link">
-                <FontAwesomeIcon
-                  icon={faHeart}
-                  style={{ color: "#eb6424", height: "25px" }}
-                />
-                <span className="tooltip-text">Favorites</span>
-              </Link>
+              <FontAwesomeIcon icon={faBell} />
             </div>
 
-            <div className="dropdown-wrapper" ref={dropdownRef}>
-              <div className="dropdown-button" onClick={toggleOpen}>
-                {notifications.length > 0 && (
-                  <span className="notification">({notifications.length})</span>
-                )}
-                <FontAwesomeIcon icon={faBell} style={{ height: "25px" }} />
-                {isOpen && (
-                  <div className="dropdown">
-                    <h2>Notifications</h2>
-                    {notifications.length > 0 ? (
-                      <ul>
-                        {notifications.map((notification) => (
-                          <li key={notification.notification_id}>
-                            {notification.message}
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p>Nothing expiring yet!</p>
-                    )}
-                  </div>
+            {notifOpen && (
+              <div className={classes.dropdown}>
+                <h4>Notifications</h4>
+                {notifications.length > 0 ? (
+                  <ul>
+                    {notifications.map((n) => (
+                      <li key={n.notification_id}>{n.message}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>Nothing expiring yet!</p>
                 )}
               </div>
-            </div>
+            )}
           </div>
+          <button
+            className={classes.hamburger}
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </button>
         </div>
-      </header>
-    </Box>
+      </div>
+
+      {menuOpen && (
+        <nav className={classes.mobileMenu}>
+          {user ? (
+            <>
+              <p>
+                Welcome, <strong>{user.username}</strong>
+              </p>
+              <Link to="/logout" className={classes.logOut}>
+                Logout
+              </Link>
+            </>
+          ) : (
+            <p>You are not logged in.</p>
+          )}
+          <Link to="/Home" className={classes.menuItem}>Home</Link>
+          <Link to="/Favorites" className={classes.menuItem}>
+            Favorites
+          </Link>
+          <Link to="/Compost" className={classes.menuItem}>
+            Compost Locations
+          </Link>
+        </nav>
+      )}
+    </header>
   );
 }
