d9aaafb45c465323b25217a5a51090a3e378e042
Lindsey <lawsonlindsey36@yahoo.com>
2025-08-21 14:38:08 -0400

    added pop up for creating and joining household, added settings page

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import SimpleHeader from "./Header";
 import DevBanner from "./DevBanner";
+import HouseholdOnboarding from "./HouseholdPopUp";
 
 export default function Layout() {
   return (
     <>
       <SimpleHeader />
+      <HouseholdOnboarding />
       <Outlet />
     </>
   );
