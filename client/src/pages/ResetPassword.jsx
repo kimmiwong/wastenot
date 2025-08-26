936adca6e465c7c96d138bcc8fae2b9a87774a74
Kimmi <189289679+kimmiwong@users.noreply.github.com>
2025-08-25 23:17:34 -0400

    added some useState variables to ResetPassword

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react"
 export default function ResetPassword() {
+    const [username, setUsername] = useState("");
+    const [question, setQuestion] = useState("");
+    const [securityAnswer, setSecurityAnswer] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     return (
     <p>Enter email</p>)
